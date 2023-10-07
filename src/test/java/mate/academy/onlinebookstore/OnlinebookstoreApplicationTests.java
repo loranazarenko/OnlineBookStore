88f72fca716e202803405a96ace77434cb430a98
loranazarenko <loranazarenko26@gmail.com>
2023-10-08 01:17:45 +0300

    First commit

--- src/test/java/mate/academy/onlinebookstore/OnlinebookstoreApplicationTests.java
+++ src/test/java/mate/academy/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -3,7 +3,7 @@ package mate.academy.onlinebookstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OnlinebookstoreApplicationTests {
 
 	@Test
