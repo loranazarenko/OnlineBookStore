26d37bda2d055c9d2343fc34fce4a13a799aa141
loranazarenko <loranazarenko26@gmail.com>
2023-10-08 12:54:39 +0300

    First commit

--- src/main/java/mate/academy/onlinebookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/BookRepository.java
@@ -2,9 +2,7 @@ package mate.academy.onlinebookstore.repository;
 
 import java.util.List;
 import mate.academy.onlinebookstore.entity.Book;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookRepository {
     Book save(Book book);
 
--- src/test/java/mate/academy/onlinebookstore/OnlinebookstoreApplicationTests.java
+++ src/test/java/mate/academy/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -1,9 +1,7 @@
 package mate.academy.onlinebookstore;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
 class OnlinebookstoreApplicationTests {
 
 	@Test
