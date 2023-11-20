ffa861200bbf019d7c39d83a3207b14df23c5c53
loranazarenko <loranazarenko26@gmail.com>
2023-11-20 23:08:14 +0200

    Done task Error Processing

--- src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,12 +1,20 @@
 package mate.academy.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import mate.academy.onlinebookstore.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @Isbn
     private String isbn;
+    @NotNull
+    @Min(value = 0)
     private Double price;
     private String description;
     private String coverImage;
